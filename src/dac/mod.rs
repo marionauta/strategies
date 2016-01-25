6a086a012f4002b249e5e235b7f39ee0975483e1
Mario Nachbaur <marionauta@users.noreply.github.com>
2016-01-25 16:55:50 +0100

    Write documentation

--- src/dac/mod.rs
+++ src/dac/mod.rs
@@ -1,20 +1,73 @@
-//! # Divide & Conquer
+//! The Divide & Conquer recursive strategy.
 //!
-//! Everything related to Divide & Conquer problems.
+//! From [Wikipedia][1]:
+//! > A divide and conquer algorithm works by recursively breaking down a
+//! > problem into two or more sub-problems of the same (or related) type
+//! > (**divide**) until these become simple enough to be solved directly
+//! > (**conquer**). The solution to the sub-problems are then combined
+//! > to give a solution to the original problem.
+//!
+//! To use this strategy you need to implement the [`DacProblem`][2] trait and
+//! everything else is handled for you.
+//!
+//! [1]: https://en.wikipedia.org/wiki/Divide_and_conquer_algorithms
+//! [2]: trait.DacProblem.html
 
 use std::marker::PhantomData;
 
 /// Divide & Conquer Problem.
+///
+/// You have to implement this trait in order to use the algorithm. Simply fill
+/// all the methods and you're all set. We have two generic types:
+///
+/// - `E`: A partial solution, wich we work with.
+/// - `S`: The final solution, the output of the algorithm.
+///
+/// Sometimes `E` and `S` are the same type because we don't need conversion
+/// between the partial and the final solution (e.g. when calculating a
+/// factorial), but if we are working with a list or an array, `E` could simply
+/// be a pointer to an element and `S` the actual element.
+///
+/// Once you have defined the problem, use [`DacAlgorithm`][1] to solve it.
+///
+/// [1]: struct.DacAlgorithm.html
 pub trait DacProblem<S, E> {
+    /// The size of your problem. Usually referred to as `n`.
+    ///
+    /// It should decrease as you split the problem, otherwise the algorithm
+    /// will never stop, and we don't want that.
     fn size(&self) -> usize;
+
+    /// The moment when the problem is simple enough to be solved directly.
+    ///
+    /// Here `DacProblem::size` is the smallest.
     fn is_base_case(&self) -> bool;
+
+    /// The base case's solution.
     fn base_case_solution(&self) -> E;
+
+    /// The number of subproblems the problem has.
     fn subproblem_count(&self) -> usize;
+
+    /// Return the subproblem corresponding to the given `i`. (i.e. the first
+    /// subproblem, the second...).
+    ///
+    /// `i` is in the range [0, `DacProblem::subproblem_count`).
     fn get_subproblem(&self, i: usize) -> Self;
+
+    /// How to combine the subproblems' solutions into the current problem's
+    /// solution.
     fn combine(&self, solutions: Vec<E>) -> E;
+
+    /// Transforms the partial solution `E`, if it is possible, into a final
+    /// solution `S`.
     fn get_solution(&self, partial_solution: &E) -> Option<S>;
 }
 
+/// The problem solver.
+///
+/// Solves a divide & conquer problem without memory. Useful if we don't need
+/// to store partial solutions.
 pub struct DacAlgorithm<S, E, P: DacProblem<S, E>> {
     phan: PhantomData<S>,
     partial_solution: E,
@@ -22,6 +75,7 @@ pub struct DacAlgorithm<S, E, P: DacProblem<S, E>> {
 }
 
 impl<S, E, P: DacProblem<S, E>> DacAlgorithm<S, E, P> {
+    /// Solve the `problem` problem.
     pub fn new(problem: P) -> Self {
         let e = Self::solve(&problem);
 
@@ -46,6 +100,7 @@ impl<S, E, P: DacProblem<S, E>> DacAlgorithm<S, E, P> {
         }
     }
 
+    /// Get the final solution.
     pub fn get_solution(&self) -> Option<S> {
         self.problem.get_solution(&self.partial_solution)
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,6 @@
+//! A Rust library that implements many algorithm-solving strategies so you
+//! don't have to. You'll only need to define your problem.
+//!
+//! Each module contains a strategy and its documentation:
+
 pub mod dac;
